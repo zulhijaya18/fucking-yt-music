5858f95ed874b1dd72165a7eb65be16fb714c530
zulhijaya18 <32.zulhijaya@gmail.com>
2021-11-17 00:31:51 +0800

    update

--- main.js
+++ main.js
@@ -4,30 +4,6 @@ const usetube = require('usetube');
 const express = require('express');
 const app = express();
 const got = require("got");
-const https = require('https');
-const stream = require('stream');
-const path = require('path');
-
-function sendRes (res){
-    var filePath = path.join(__dirname, 'lagu.webm');
-    var stat = fs.statSync(filePath);
-    
-    res.writeHead(200, {
-        'Content-Type': 'audio/webm',
-        'Content-Length': stat.size,
-        'Content-Disposition': 'attachment; filename=lagu.webm'
-    });
-
-    var readStream = fs.createReadStream(filePath);
-    readStream.on('open', function() {
-        readStream.pipe(res);
-    });
-
-    readStream.on('error', function(err) {
-        res.end(err);
-    });
-}
-
 app.get('/new-yt-music/search/:keyword', async (req, res) => {
     let search = await usetube.searchVideo(req.params.keyword);
     res.send(search.videos);
@@ -37,17 +13,19 @@ app.get('/new-yt-music/search/:keyword', async (req, res) => {
 app.get('/new-yt-music/video/:videoId', async (req, res) => {
     let info = await ytdl.getInfo('https://www.youtube.com/watch?v='+req.params.videoId);
     let url = '';
+    let fileSize = 0;
+    let fileName = req.params.videoId+'.webm';
+    fs.writeFileSync('info.txt', JSON.stringify(info));
     info.formats.forEach(format => {
         if (format.itag == '249') {
             url = format.url;
+            fileSize = format.contentLength;
         }
+        // console.log(format);
     });
     
-    const fileName = "lagu.webm";
-    
     const downloadStream = got.stream(url);
-    const fileWriterStream = fs.createWriteStream(fileName);
-    
+
     downloadStream
       .on("downloadProgress", ({ transferred, total, percent }) => {
         const percentage = Math.round(percent * 100);
@@ -56,17 +34,14 @@ app.get('/new-yt-music/video/:videoId', async (req, res) => {
       .on("error", (error) => {
         console.error(`Download failed: ${error.message}`);
       });
-    
-    fileWriterStream
-      .on("error", (error) => {
-        console.error(`Could not write file to system: ${error.message}`);
-      })
-      .on("finish", () => {
-        console.log(`File downloaded to ${fileName}`);
-        sendRes(res);
+
+      res.writeHead(200, {
+        'Content-Type': 'audio/webm',
+        'Content-Length': fileSize,
+        'Content-Disposition': 'attachment; filename='+fileName
       });
-    
-    downloadStream.pipe(fileWriterStream);
+
+    downloadStream.pipe(res);
 });
 
 
